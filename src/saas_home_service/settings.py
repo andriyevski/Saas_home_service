f23d6e35c4a15b0fe966ebeeefbf0b4c9c258e28
Andriyevski Serhiy <andriyevski@gmail.com>
2024-11-03 03:16:45 +0200

    I try to change settings: STATIC_URL , STATIC_ROOT, BASE_DIR, import os

--- src/saas_home_service/settings.py
+++ src/saas_home_service/settings.py
@@ -9,9 +9,13 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
