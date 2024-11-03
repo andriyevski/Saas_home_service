dc5b4d25520942a595571f122e8ab9b56da9f519
Andriyevski Serhiy <andriyevski@gmail.com>
2024-11-03 03:22:23 +0200

    I try to change settings: WORKDIR

--- src/saas_home_service/settings.py
+++ src/saas_home_service/settings.py
@@ -16,6 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
